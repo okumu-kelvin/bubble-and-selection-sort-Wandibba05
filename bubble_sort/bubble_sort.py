926f9f24c813673858e09c12d43ce72c0bbde591
Wandibba05 <emmanuel.wandibba@strathmore.edu>
2025-06-20 09:59:16 +0300

    Assignment completed

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,14 @@
-def bubble_sort(unsorted_list):
-    # TODO: Implement bubble sort
-    pass
+def bubble_sort(arr):
+    for i in range(len(arr)):
+
+        for j in range(len(arr) - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+
+my_list = [64, 34, 25, 12, 22, 11, 90]
+print("Original list:", my_list)
+
+bubble_sort(my_list)
+print("Sorted list:  ", my_list)
+
--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,13 @@
 def selection_sort(arr):
-    # TODO: Implement selection sort
-    pass
+    for i in range(len(arr)):
+        min_idx = min(range(i, len(arr)), key=arr.__getitem__)
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
+
+my_list = [29, 10, 14, 37, 13]
+print("Original:", my_list)
+
+selection_sort(my_list)
+print("Sorted:  ", my_list)
+
+
